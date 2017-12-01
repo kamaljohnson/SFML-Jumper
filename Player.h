17bb1ee2ca5cf75d326ba20dcfba336e9817e172
Kamal Johnson <kamaljohnson12345@gmail.com>
2017-12-02 04:56:24 +0530

    collision detection for rectangles

--- Player.h
+++ Player.h
@@ -1,27 +1,54 @@
 #pragma once
 #include<SFML/Graphics.hpp>
+#include<iostream>
 
 class Player
 {
 public:
+	sf::RectangleShape body;
+	sf::Vector2f pos;
+	sf::Vector2f speed;
+	bool isJumping;
+	float maxYspeed = 0.3f;
+	float maxXspeed = 0.3f;
+private:
+	sf::Texture texture;
+	float jumpSpeed = 10.0f;
+	float gravity = 0.0005f;
+	float accn = 0.0002f;
+	float friction = 0.0003f;
+public:
 	Player(float size);
 	~Player();
-	void Jump()
+	void move(sf::RenderWindow &window)
 	{
+		if (isColliding(sf::Vector2f(100.0f, 280.0f), sf::Vector2f(100.0f, 100.0f), window))
+		{
+
+		}
+		pos.x += speed.x;
 		pos.y -= speed.y;
-		speed.y -= gravity;
-	}
-	void move()
-	{
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
 		{
-			pos.x -= speed.x;
+			if (speed.x > -maxXspeed)
+				speed.x -= accn;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		else
 		{
-			pos.x += speed.x;
+			if (speed.x < 0)
+				speed.x += friction;	//friction = getFriction() 
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space) && !isJumping)
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+		{
+			if (speed.x < maxXspeed)
+				speed.x += accn;
+		}
+		else
+		{
+			if (speed.x > 0)
+				speed.x -= friction;
+		}
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)|| sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))&& !isJumping)
 		{
 			speed.y = maxYspeed;
 			isJumping = true;
@@ -30,22 +57,56 @@ public:
 		{
 			Jump();
 			if (speed.y <= -maxYspeed)	//this code must be changed to collision test
+			{
+				speed.y = 0;
 				isJumping = false;
+			}
 		}
-
 		body.setPosition(pos);
 	}
-
-public:
-	sf::RectangleShape body;
-	sf::Vector2f pos;
-	sf::Vector2f speed = sf::Vector2f(0.1f, 0.1f);
-	bool isJumping;
-	float maxXspeed = 1.0f;
-	float maxYspeed = 0.50f;
 private:
-	sf::Texture texture;
-	float jumpSpeed = 10.0f;
-	float gravity = 0.001f;
+	void Jump()
+	{
+		speed.y -= gravity;
+	}
+	bool isColliding(sf::Vector2f pos1, sf::Vector2f size, sf::RenderWindow &window)
+	{
+		sf::Vector2f pos2 = pos1 + size;
+		sf::RectangleShape rect(size);
+		rect.setPosition(pos1);
+		rect.setFillColor(sf::Color::Blue);
+		window.draw(rect);
+		//pos1 is the top left cornner one and pos2 is the bottom right one 
+		float lx, sx, ly, sy;
+		if (pos1.x > pos2.x)
+		{
+			lx = pos1.x;
+			sx = pos2.x;
+		}
+		else
+		{
+			lx = pos2.x;
+			sx = pos1.x;
+		}
+		if (pos1.y > pos2.y)
+		{
+			ly = pos1.y;
+			sy = pos2.y;
+		}
+		else
+		{
+			ly = pos2.y;
+			sy = pos1.y;
+		}
+		sf::Vector2f s = body.getSize();
+		if (pos.x < lx && pos.x + s.x > sx && pos.y < ly && pos.y + s.y > sy)
+		{
+			rect.setFillColor(sf::Color::Cyan);
+			window.draw(rect);
+			return(true);
+		}
+		else
+			return(false);
+	}
 };
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,19 @@
 #include<iostream>
-#include<SFML/Graphics.hpp>
+#include<SFML\Graphics.hpp>
 #include "Player.h"
 
 void main()
 {
-	Player player(10.0f);
+	Player player(40.0f);
 
 	sf::RenderWindow window(sf::VideoMode(640, 480), "Jumper", sf::Style::Resize | sf::Style::Close);
 
-
-	while (window.isOpen())
+	while (window.isOpen())	//the game loop
 	{
 		sf::Event evnt;
 
 		while (window.pollEvent(evnt))
 		{
-
 			switch (evnt.type)
 			{
 			case sf::Event::Closed:
@@ -23,10 +21,13 @@ void main()
 				break;
 			}
 		}
+		//clearing the screen
 		window.clear();
 
-		player.move();
+		//game logic
+		player.move(window);
 
+		//rendering the screen
 		window.draw(player.body);
 		window.display();
 	}
